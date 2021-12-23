9607be6d8e6b3c2cd2bf2aa01e00db6131a0e85c
Shakib88 <shakibeeeuap28@gmail.com>
2021-12-23 14:50:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// console.log("Bimillah"); //Bimillah
+// console.log("Alhamdulillahi Rabbil Alamin"); //Alhamdulillahi Rabbil Alamin
+// console.log(1); //1
+// console.log(1.33); //1.33
+
+//===================================================
+
+//string+number=string
+
+// console.log('My favourite Number is: ' + 1); //My favourite Number is: 1
+
+//====================================================
+
+//STRING 
+
+// console.log(typeof 4.5)//Number
+// console.log(typeof "x") // Number
+
+//=====================================================
+// MINUS IS ALSO A NUMBER
+
+//console.log(- (10 - 2)); // 8
+
+//================================================
+
+// Variable can use other part
+
+// var x= "is a student"
+// var y= 26
+
+// console.log("Shakib" + x);
+// console.log('I am '+ y );
+
+//=================================================
+
+
+
+
+
+
+
+
+
